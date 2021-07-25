125701e180c53eaa228ef30cb85023e263934c85
thed4mir <zhanzakovdamir2000@gmail.com>
2021-07-25 15:43:43 +0500

    Database Changes

--- src/Project/Database.java
+++ src/Project/Database.java
@@ -1,13 +1,15 @@
 package Project;
 
 public class Database {
-    public static BankAccount allAccounts[] = new BankAccount[100];
+    public static BankAccount[] allAccounts = new BankAccount[100];
     static {
         allAccounts[0] = new CityBankAccount("Ilyas", "Zhuanyshev", 20000, "KZ010322312", "0102");
         allAccounts[1] = new CityBankAccount("Erbol", "Assanbek", 10000, "KZ010322313", "0101");
         allAccounts[2] = new NationalBankAccount("Ilyas Zhuanyshev", 2000, "KZ0101112", "0102");
+        allAccounts[3] = new NationalBankAccount("Yoel Romero", 3000, "KZ0101113", "1234");
     }
-    private int index = 3;
+    private int index = 4;
+
     public void addAccount(BankAccount bankAccount){
         allAccounts[index] = bankAccount;
         index++;
